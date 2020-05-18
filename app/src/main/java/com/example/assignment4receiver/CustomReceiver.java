984a3333d3a85c24c45b46dd3afa024e628126af
hari <harikrishnanama1500@gmail.com>
2020-05-18 07:19:08 -0400

    Explicit Broadcast Receivers added

--- app/src/main/java/com/example/assignment4receiver/CustomReceiver.java
+++ app/src/main/java/com/example/assignment4receiver/CustomReceiver.java
@@ -6,12 +6,11 @@ import android.content.Intent;
 import android.widget.Toast;
 
 public class CustomReceiver extends BroadcastReceiver {
-    public static final String intentAction = "com.assignment4.ACTION_SEND_MESS";
-    public static final String messageKey = "com.assignment4.ACTION_SEND_MESS.MESS";
+    public static final String intentAction = "com.assignment.ACTION_SEND_MESS";
+    public static final String messageKey = "com.assignment.ACTION_SEND_MESS.MESS";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "onReceive", Toast.LENGTH_LONG).show();
         if (intentAction.equals(intent.getAction())) {
             String receivedMessage = intent.getStringExtra(messageKey);
             Toast.makeText(context, receivedMessage, Toast.LENGTH_LONG).show();
