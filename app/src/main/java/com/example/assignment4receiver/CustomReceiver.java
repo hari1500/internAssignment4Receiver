639c9f5eac52e73419fb2ad6d0b927a0410e070a
hari <harikrishnanama1500@gmail.com>
2020-05-18 06:43:36 -0400

    Explicit Broadcast Receivers added

--- app/src/main/java/com/example/assignment4receiver/CustomReceiver.java
+++ app/src/main/java/com/example/assignment4receiver/CustomReceiver.java
@@ -8,10 +8,10 @@ import android.widget.Toast;
 public class CustomReceiver extends BroadcastReceiver {
     public static final String intentAction = "com.assignment4.ACTION_SEND_MESS";
     public static final String messageKey = "com.assignment4.ACTION_SEND_MESS.MESS";
-    public static final String intentPermission = "com.assignment4.PERMISSION_HARI";
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "onReceive", Toast.LENGTH_LONG).show();
         if (intentAction.equals(intent.getAction())) {
             String receivedMessage = intent.getStringExtra(messageKey);
             Toast.makeText(context, receivedMessage, Toast.LENGTH_LONG).show();
--- app/src/main/java/com/example/assignment4receiver/MainActivity.java
+++ app/src/main/java/com/example/assignment4receiver/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.assignment4receiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    CustomReceiver customReceiver = new CustomReceiver();
+    IntentFilter intentFilter = new IntentFilter(CustomReceiver.intentAction);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        registerReceiver(customReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(customReceiver);
     }
 }
